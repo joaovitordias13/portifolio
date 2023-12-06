c45fb43223a2dc1345453c643a69ff1751f72bf4
Joao Vitor Ribeiro <joaovitordias.2b@gmail.com>
2023-12-05 22:37:54 -0300

    configurando o ambiente de desenvolvimento

---
+++ src/Teste.tsx
@@ -0,0 +1,38 @@
+import styled from 'styled-components'
+
+type BotaoProps = {
+  principal: boolean
+  fontSize: string
+}
+
+const Botao = styled.button<BotaoProps>`
+  background-color: ${(props) => (props.principal ? 'green' : 'blue')};
+  font-size: ${(props) => props.fontSize || '16px'};
+`
+
+const BotaoPerigo = styled(Botao)`
+  background-color: red;
+  color: #fff;
+
+  span {
+    text-decoration: line-through;
+  }
+`
+
+function Teste() {
+  return (
+    <>
+      <Botao fontSize="18px" principal>
+        Enviar
+      </Botao>
+      <Botao fontSize="14px" principal={false}>
+        Cancelar
+      </Botao>
+      <BotaoPerigo fontSize="14px" as="a" principal>
+        Não clique aqui
+      </BotaoPerigo>
+    </>
+  )
+}
+
+export default Teste
---
+++ src/styles.ts
@@ -0,0 +1,9 @@
+import { createGlobalStyle } from 'styled-components'
+
+const EstiloGlobal = createGlobalStyle`
+  *{
+    margin:0;
+    padding:0;
+  }
+`
+export default EstiloGlobal
